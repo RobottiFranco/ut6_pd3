0f173f24154c80824d6d1ce0a0fe6ea08921cfdc
RobottiFranco <franco.robotti0@gmail.com>
2024-05-25 03:22:14 -0300

    ejercicio 2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,14 +6,14 @@ import java.util.Iterator;
 
 public final class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         HashMap<String, String> hashMap = new HashMap<String, String>();
-        hashMap.put("Galton", "Galton");
-        hashMap.put("Juan", "Juan");
+        hashMap.put("Galton", "notlag");
+        hashMap.put("Juan", "nuaj");
         hashMap.put(null, null);
-        hashMap.put("Julio", "Julio");
-        hashMap.put("Maria", "Maria");
+        hashMap.put("Julio", "oiluj");
+        hashMap.put("Maria", "airam");
         hashMap.put("null", null);
 
         System.out.println("Antes: " + hashMap);
@@ -21,6 +21,28 @@ public final class App {
         eiminarNulos(hashMap);
 
         System.out.println("Después: " + hashMap);
+
+        Map<String, String> invertido = invertirMapa(hashMap);
+
+        System.out.println("invertido:" + invertido);
+    }
+
+
+    public static Map<String, String> invertirMapa(Map<String, String> hashMap) throws Exception {
+        Map<String, String> invertedMap = new HashMap<>();
+
+        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+            String originalKey = entry.getKey();
+            String originalValue = entry.getValue();
+
+            if (invertedMap.containsKey(originalValue)) {
+                throw new Exception("Valor duplicado encontrado: " + originalValue);
+            }
+
+            invertedMap.put(originalValue, originalKey);
+        }
+
+        return invertedMap;
     }
 
     public static void eiminarNulos(HashMap hashMap) {
